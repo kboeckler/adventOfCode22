c3d9d85a04ec4964ec6b302ec7c6d93005444f0a
kbo <Kevin.Boeckler@mach.de>
2022-12-09 09:33:33 +0100

    Day 9 complete

--- solution/day9.go
+++ solution/day9.go
@@ -14,14 +14,48 @@ type day9 struct {
 }
 
 func (d day9) SolvePart1(input []string) interface{} {
-	moves := make([]*move, 0)
-	for _, row := range input {
-		split := strings.Split(row, " ")
-		steps, _ := strconv.Atoi(split[1])
-		for i := 0; i < steps; i++ {
-			moves = append(moves, &move{split[0]})
-		}
+	return d.solveRopeWith(input, 2)
+}
+
+func (d day9) SolvePart2(input []string) interface{} {
+	return d.solveRopeWith(input, 10)
+}
+
+func (d day9) solveRopeWith(input []string, amountKnots int) interface{} {
+	moves := d.parseMoves(input)
+	dim := d.findDimension(moves)
+	tailPositions := make(map[int]bool)
+	knots := make([]int, 0, amountKnots)
+	for i := 0; i < amountKnots; i++ {
+		knotX := -dim.negativeWidthOffset
+		knotY := -dim.negativeHeightOffset
+		knots = append(knots, d.coordToIndex(knotX, knotY, dim.width))
 	}
+	tailPositions[knots[len(knots)-1]] = true
+	for _, move := range moves {
+		headX, headY := d.indexToCoord(knots[0], dim.width)
+		headX, headY = move.nextHead(headX, headY)
+		knots[0] = d.coordToIndex(headX, headY, dim.width)
+		for i := 0; i < len(knots)-1; i++ {
+			leaderX, leaderY := d.indexToCoord(knots[i], dim.width)
+			followerX, followerY := d.indexToCoord(knots[i+1], dim.width)
+			followerX, followerY = d.updateKnotPosition(leaderX, leaderY, followerX, followerY)
+			knots[i+1] = d.coordToIndex(followerX, followerY, dim.width)
+		}
+		tailPositions[knots[len(knots)-1]] = true
+	}
+	return len(tailPositions)
+}
+
+type dimension struct {
+	width, height, negativeWidthOffset, negativeHeightOffset int
+}
+
+type move struct {
+	direction string
+}
+
+func (d day9) findDimension(moves []*move) dimension {
 	currentWidth := 1
 	negativeWidth := 1
 	width := currentWidth
@@ -56,30 +90,28 @@ func (d day9) SolvePart1(input []string) interface{} {
 	negativeHeightOffset := negativeHeight - 1
 	width -= negativeWidthOffset
 	height -= negativeHeightOffset
-	tailPositions := make(map[int]bool)
-	headX := -negativeWidthOffset
-	headY := -negativeHeightOffset
-	tailX := -negativeWidthOffset
-	tailY := -negativeHeightOffset
-	tailPositions[d.coordToIndex(tailX, tailY, width)] = true
-	for _, move := range moves {
-		headX, headY = move.nextHead(headX, headY)
-		tailX, tailY = d.udateTail(headX, headY, tailX, tailY)
-		tailPositions[d.coordToIndex(tailX, tailY, width)] = true
-	}
-	return len(tailPositions)
+	dim := dimension{width, height, negativeWidthOffset, negativeHeightOffset}
+	return dim
 }
 
-func (d day9) SolvePart2(input []string) interface{} {
-	return ""
+func (d day9) parseMoves(input []string) []*move {
+	moves := make([]*move, 0)
+	for _, row := range input {
+		split := strings.Split(row, " ")
+		steps, _ := strconv.Atoi(split[1])
+		for i := 0; i < steps; i++ {
+			moves = append(moves, &move{split[0]})
+		}
+	}
+	return moves
 }
 
 func (d day9) coordToIndex(x, y, width int) int {
 	return y*width + x
 }
 
-type move struct {
-	direction string
+func (d day9) indexToCoord(index, width int) (int, int) {
+	return index % width, index / width
 }
 
 func (m *move) nextHead(hx, hy int) (int, int) {
@@ -96,25 +128,25 @@ func (m *move) nextHead(hx, hy int) (int, int) {
 	panic("Invalid direction in move: " + m.direction)
 }
 
-func (d day9) udateTail(hx, hy, tx, ty int) (int, int) {
-	headTailDist2 := d.euclideanDist2(hx, hy, tx, ty)
+func (d day9) updateKnotPosition(leaderX, leaderY, followerX, followerY int) (int, int) {
+	headTailDist2 := d.euclideanDist2(leaderX, leaderY, followerX, followerY)
 	if headTailDist2 >= 4.0 {
 		newMinDist2 := 4.0
 		bestX := 0
 		bestY := 0
 		for dx := -1; dx <= 1; dx++ {
 			for dy := -1; dy <= 1; dy++ {
-				dist2 := d.euclideanDist2(hx, hy, tx+dx, ty+dy)
+				dist2 := d.euclideanDist2(leaderX, leaderY, followerX+dx, followerY+dy)
 				if dist2 < newMinDist2 {
 					newMinDist2 = dist2
-					bestX = tx + dx
-					bestY = ty + dy
+					bestX = followerX + dx
+					bestY = followerY + dy
 				}
 			}
 		}
 		return bestX, bestY
 	}
-	return tx, ty
+	return followerX, followerY
 }
 
 func (d day9) euclideanDist2(ax, ay, bx, by int) float64 {
