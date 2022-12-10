97ef925ebf856b4e3a33ea80531a475f0f7c6d88
Kevin Böckler <kevinboeckler@gmail.com>
2022-12-10 09:28:12 +0100

    Day 10

---
+++ solution/day10.go
@@ -0,0 +1,58 @@
+package solution
+
+import (
+	"math"
+	"strconv"
+	"strings"
+)
+
+func init() {
+	RegisterSolution(10, day10{})
+}
+
+type day10 struct {
+}
+
+func (d day10) SolvePart1(input []string) interface{} {
+	xValues := d.calculateXValuesPerCycle(input)
+	sum := 0
+	for i := 20; i <= 220; i = i + 40 {
+		sum += xValues[i-1] * i
+	}
+	return sum
+}
+
+func (d day10) SolvePart2(input []string) interface{} {
+	xValues := d.calculateXValuesPerCycle(input)
+	crt := strings.Builder{}
+	crt.WriteString("\n")
+	for i := 0; i < 240; i++ {
+		column := i % 40
+		distanceSprite := int(math.Abs(float64(column - xValues[i])))
+		if distanceSprite <= 1 {
+			crt.WriteString("#")
+		} else {
+			crt.WriteString(".")
+		}
+		if column == 39 {
+			crt.WriteString("\n")
+		}
+	}
+	return crt.String()
+}
+
+func (d day10) calculateXValuesPerCycle(input []string) []int {
+	xValues := make([]int, 0)
+	xValues = append(xValues, 1)
+	for _, row := range input {
+		if strings.EqualFold(row, "noop") {
+			xValues = append(xValues, xValues[len(xValues)-1])
+		} else {
+			split := strings.Split(row, " ")
+			summand, _ := strconv.Atoi(split[1])
+			xValues = append(xValues, xValues[len(xValues)-1])
+			xValues = append(xValues, xValues[len(xValues)-1]+summand)
+		}
+	}
+	return xValues
+}
